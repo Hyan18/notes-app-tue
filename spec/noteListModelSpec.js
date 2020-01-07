8ae7708efaf5b09e1df76f1e351338055e521455
Hyan18 <Hyan18@github.com>
2020-01-07 12:12:54 +0000

    remove commented code

--- spec/noteListModelSpec.js
+++ spec/noteListModelSpec.js
@@ -1,17 +1,5 @@
-// (function (exports) {
-//   function createNoteAddsNewNoteToList () {
-//     var noteList = new NoteList()
-//     noteList.createNote('this is a note')
-
-//     return assert.isTrue(noteList.showNotes()[0] instanceof Note)
-//   }
-//   exports.createNoteAddsNewNoteToList = createNoteAddsNewNoteToList
-// })(this)
-
-// console.log(createNoteAddsNewNoteToList())
-
 describe('NoteList', () => {
-  describe('.createNote', () => {
+  describe('#createNote', () => {
     it('adds new note to a list', () => {
       var noteList = new NoteList()
       noteList.createNote('this is a note')
